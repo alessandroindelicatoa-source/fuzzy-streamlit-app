2f2113f5dd58b85bde545d533c10228cd2384c94
alessandroindelicatoa-source <alessandro.indelicato.a@gmail.com>
2025-09-18 17:24:58 +0200

    Add files via upload

--- app_fuzzy.py
+++ app_fuzzy.py
@@ -1,3 +1,3 @@
 # app_fuzzy.py
 # -*- coding: utf-8 -*-
-# (El código completo con probability ratios va aquí, igual que lo mostré en el mensaje anterior)
+# (el código completo que generamos antes)
